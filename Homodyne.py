f3085d3f479ac21182f33adbd6dba8c7d32597e3
taladjidi <taladjidi@gmail.com>
2021-02-02 10:28:30 +0100

    Calib fringes sweep

--- Homodyne.py
+++ Homodyne.py
@@ -34,13 +34,15 @@ class Homodyne:
         self.cam = cam
 
     def calib_fringes(self, channel: int = 1, start: int = 0,
-                      stop: int = 10000, steps: int = 200) -> np.ndarray:
+                      stop: int = 10000, steps: int = 200,
+                      pxpitch: float = 5.5e-6) -> np.ndarray:
         """Function to calibrate the k values accessed by the local oscillator
 
         :param PiezoTIM101 piezo: piezo to actuate
         :param int channel: Piezo channel to actuate
         :param int start: Start position of the piezo
         :param int stop: Stop position of the piezo
+        :param float pxpitch: Pixel pitch of the camera
         :return: Description of returned object.
         :rtype: np.ndarray
 
@@ -48,6 +50,8 @@ class Homodyne:
         # Gets the camera size
         h = int(self.cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
         w = int(self.cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+        kx = np.fft.fftfreq(w, pxpitch)
+        ky = np.fft.fftfreq(h, pxpitch)
         ret = False
         counter = 0
         while not(ret) and counter < 10:
@@ -62,9 +66,19 @@ class Homodyne:
         pos_range = np.linspace(start, stop, steps)
         positions = np.empty(pos_range.shape)
         frames = np.empty((h, w, len(pos_range))
+        frames_fft = np.empty((h, w, len(pos_range))
         fig = plt.figure(0)
-        ax = fig.add_subplot(111)
-        im = ax.imshow(np.ones((frames.shape[0], frames.shape[1]), cmap="gray")
+        ax0 = fig.add_subplot(121)
+        ax1 = fig.add_subplot(122)
+        ax0.set_title("Image")
+        ax1.set_title("Fourier transform")
+        im0 = ax.imshow(np.ones((frames.shape[0], frames.shape[1]),
+                        cmap="gray")
+        im1 = ax.imshow(np.ones((frames.shape[0], frames.shape[1]))
+        ax1.set_xlabel("$k_x$ in $\\mu m^{-1}$")
+        ax1.set_ylabel("$k_y$ in $\\mu m^{-1}$")
+        ax1.set_xticks(kx*1e-6)
+        ax1.set_yticks(ky*1e-6)
         # jogs along the specified range
         for counter, pos in enumerate(pos_range):
             positions[counter] = self.piezo.move_to(channel, pos)
@@ -76,8 +90,11 @@ class Homodyne:
             if not(ret):
                 print("ERROR : Could not grab frame")
                 sys.exit()
-            im.set_data(frames[:, :, counter])
-            ax.set_title(f"Grabbed frame {counter+1}/{len(pos_range)}")
+            frames_fft[:, :, counter] = np.fft.fftshift(
+                                            np.fft.fft2(frames[:, :, counter]))
+            im0.set_data(frames[:, :, counter])
+            im1.set_data(frames_fft[:, :, counter])
+            fig.suptitle(f"Grabbed frame {counter+1}/{len(pos_range)}")
             fig.canvas.draw()
         plt.show(block=False)
         # returns piezo to original position
