c562b6aa534dca58713fd78610d93cdd07e3b1e8
Tangui Aladjidi <taladjidi@gmail.com>
2021-02-02 16:00:34 +0100

    Update homodyne_exp.py

--- homodyne_exp.py
+++ homodyne_exp.py
@@ -5,15 +5,28 @@ import matplotlib.pyplot as plt
 from ScopeInterface import USBScope, USBSpectrumAnalyzer
 from piezo import PiezoTIM101
 from Homodyne import Homodyne
+import os
 
-piezo = PiezoTIM101('65863991')
-piezo.zero()
-piezo.move(channel=1, pos=100)
-piezo.disconnect()
+plt.switch_backend('Qt5Agg')
+plt.ion()
 
-piezo = PiezoTIM101('65863991')
+# piezo = PiezoTIM101('65863991')
+# piezo.move_to(channel=1, pos=-100)
+# piezo.move_to(channel=1, pos=0)
+# piezo.disconnect()
+
 cam = EasyPySpin.VideoCapture(0)
-scope = USBScope()
+scope = None
 specAn = USBSpectrumAnalyzer(addr="USB0::6833::2400::DSA8A223200862::0::INSTR")
+piezo = PiezoTIM101('65863991')
+pxpitch = 5.5e-6 
+h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+kx = np.fft.fftfreq(w, pxpitch)*1e-6
+ky = np.fft.fftfreq(h, pxpitch)*1e-6
 setup = Homodyne(piezo, specAn, scope, cam)
-setup.calib_fringes()
+frames, positions = setup.calib_fringes(start=100, stop=10000, steps=200)
+
+piezo.disconnect()
+cam.release()
+specAn.close()
